40bf7b4be495332ee29b75f68e1b22b147e4ee6e
kopja3 <kopja3@gmail.com>
2026-03-29 18:04:11 +0300

    Sounds on

--- main.ts
+++ main.ts
@@ -1,42 +1,30 @@
-let groundRef = 0
-let groundNow = 0
-
 let leftFront = 0
 let centerFront = 0
 let rightFront = 0
-
 let leftSpeed = 0
 let rightSpeed = 0
-
 let pidForwardActive = false
 let forwardStartedAt = 0
 let stuckSince = 0
 let almostStoppedSince = 0
-
 let turnBias = 1
 
-let LIDAR_ADDR = matrixLidarDistance.Addr.Addr4
+let lastAnnounceTime = 0
+let ANNOUNCE_COOLDOWN_MS = 1500
 
-// Jos auto kääntyy väärään suuntaan, vaihda tämä arvo -1:ksi
-let RIGHT_SIGN = 1
+const LIDAR_ADDR = matrixLidarDistance.Addr.Addr4
+const RIGHT_SIGN = 1
 
-// Säädöt
-let CLIFF_MARGIN_MM = 50
-let SIDE_OBSTACLE_MM = 260
-let CENTER_OBSTACLE_MM = 220
-
-let FORWARD_SEGMENT_CM = 120
-let BACKUP_CM = 15
-let CLIFF_BACKUP_CM = 10
-
-let SMALL_TURN_DEG = 25
-let NORMAL_TURN_DEG = 95
-let ESCAPE_TURN_DEG = 130
-
-let STUCK_SPEED_CM_S = 2
-let STUCK_TIME_MS = 3000
-let FINISHED_SPEED_CM_S = 1
-let FINISHED_TIME_MS = 400
+const SIDE_OBSTACLE_MM = 260
+const CENTER_OBSTACLE_MM = 220
+const FORWARD_SEGMENT_CM = 120
+const BACKUP_CM = 15
+const NORMAL_TURN_DEG = 95
+const ESCAPE_TURN_DEG = 130
+const STUCK_SPEED_CM_S = 2
+const STUCK_TIME_MS = 3000
+const FINISHED_SPEED_CM_S = 1
+const FINISHED_TIME_MS = 400
 
 function avgSpeed() {
     return (Math.abs(leftSpeed) + Math.abs(rightSpeed)) / 2
@@ -58,10 +46,6 @@ function readFrontDistances() {
     leftFront = matrixLidarDistance.matrixPointOutput(LIDAR_ADDR, 0, 6)
     centerFront = matrixLidarDistance.matrixPointOutput(LIDAR_ADDR, 3, 6)
     rightFront = matrixLidarDistance.matrixPointOutput(LIDAR_ADDR, 7, 6)
-}
-
-function readCliffDistance() {
-    groundNow = matrixLidarDistance.matrixPointOutput(LIDAR_ADDR, 3, 3)
 }
 
 function stopPidNow() {
@@ -97,18 +81,82 @@ function turnLeftDeg(deg: number) {
     )
 }
 
-function chooseOpenTurn(deg: number) {
+function canAnnounce() {
+    return input.runningTime() - lastAnnounceTime > ANNOUNCE_COOLDOWN_MS
+}
+
+function announceObstacleLeft() {
+    if (canAnnounce()) {
+        lastAnnounceTime = input.runningTime()
+        basic.showString("ESTE VAS")
+        music.playTone(262, music.beat(BeatFraction.Half))
+    }
+}
+
+function announceObstacleRight() {
+    if (canAnnounce()) {
+        lastAnnounceTime = input.runningTime()
+        basic.showString("ESTE OIK")
+        music.playTone(262, music.beat(BeatFraction.Half))
+    }
+}
+
+function announceStuckLeft() {
+    if (canAnnounce()) {
+        lastAnnounceTime = input.runningTime()
+        basic.showString("JUMI VAS")
+        music.playTone(330, music.beat(BeatFraction.Quarter))
+        basic.pause(60)
+        music.playTone(330, music.beat(BeatFraction.Quarter))
+        basic.pause(60)
+        music.playTone(330, music.beat(BeatFraction.Quarter))
+    }
+}
+
+function announceStuckRight() {
+    if (canAnnounce()) {
+        lastAnnounceTime = input.runningTime()
+        basic.showString("JUMI OIK")
+        music.playTone(330, music.beat(BeatFraction.Quarter))
+        basic.pause(60)
+        music.playTone(330, music.beat(BeatFraction.Quarter))
+        basic.pause(60)
+        music.playTone(330, music.beat(BeatFraction.Quarter))
+    }
+}
+
+function chooseOpenTurn(deg: number, reason: string) {
     readFrontDistances()
 
     if (spaceScore(leftFront) > spaceScore(rightFront) + 40) {
+        if (reason == "ESTE") {
+            announceObstacleLeft()
+        } else if (reason == "JUMI") {
+            announceStuckLeft()
+        }
         turnLeftDeg(deg)
     } else if (spaceScore(rightFront) > spaceScore(leftFront) + 40) {
+        if (reason == "ESTE") {
+            announceObstacleRight()
+        } else if (reason == "JUMI") {
+            announceStuckRight()
+        }
         turnRightDeg(deg)
     } else {
         if (turnBias > 0) {
+            if (reason == "ESTE") {
+                announceObstacleRight()
+            } else if (reason == "JUMI") {
+                announceStuckRight()
+            }
             turnRightDeg(deg)
             turnBias = -1
         } else {
+            if (reason == "ESTE") {
+                announceObstacleLeft()
+            } else if (reason == "JUMI") {
+                announceStuckLeft()
+            }
             turnLeftDeg(deg)
             turnBias = 1
         }
@@ -126,7 +174,7 @@ function obstacleEscape() {
     )
     basic.pause(60)
 
-    chooseOpenTurn(NORMAL_TURN_DEG)
+    chooseOpenTurn(NORMAL_TURN_DEG, "ESTE")
     basic.pause(60)
 }
 
@@ -141,107 +189,42 @@ function stuckEscape() {
     )
     basic.pause(60)
 
-    chooseOpenTurn(ESCAPE_TURN_DEG)
+    chooseOpenTurn(ESCAPE_TURN_DEG, "JUMI")
     basic.pause(60)
 }
 
-function cliffEscape() {
-    stopPidNow()
-    basic.pause(60)
-
-    maqueenPlusV2.pidControlDistance(
-        maqueenPlusV2.SpeedDirection.SpeedCCW,
-        CLIFF_BACKUP_CM,
-        maqueenPlusV2.MyInterruption.NotAllowed
-    )
-    basic.pause(60)
-
-    if (turnBias > 0) {
-        turnRightDeg(110)
-        turnBias = -1
-    } else {
-        turnLeftDeg(110)
-        turnBias = 1
-    }
-
-    basic.pause(60)
-}
-
-function calibrateGround() {
-    let sum = 0
-    let count = 0
-    let d = 0
-
-    // LiDAR tarvitsee noin 3 s käynnistyäkseen
-    basic.pause(3500)
-
-    for (let i = 0; i < 8; i++) {
-        d = matrixLidarDistance.matrixPointOutput(LIDAR_ADDR, 3, 3)
-        if (d > 0) {
-            sum += d
-            count += 1
-        }
-        basic.pause(30)
-    }
-
-    if (count > 0) {
-        groundRef = sum / count
-    } else {
-        groundRef = 120
-    }
-}
+music.setBuiltInSpeakerEnabled(true)
 
 maqueenPlusV2.I2CInit()
 matrixLidarDistance.initialize(LIDAR_ADDR, matrixLidarDistance.Matrix.MAT)
-calibrateGround()
+
+// LiDAR tarvitsee noin 3 s käynnistyä
+basic.pause(3500)
+
+// Käynnistä heti ensimmäinen eteenajo
+startForwardPid()
 
 basic.forever(function () {
     let actionTaken = false
 
     readFrontDistances()
-    readCliffDistance()
 
     leftSpeed = maqueenPlusV2.readRealTimeSpeed(maqueenPlusV2.DirectionType2.Left)
     rightSpeed = maqueenPlusV2.readRealTimeSpeed(maqueenPlusV2.DirectionType2.Right)
 
-    // 1) Rapun/pudotuksen tunnistus
-    if (groundNow > 0 && groundNow > groundRef + CLIFF_MARGIN_MM) {
-        cliffEscape()
-        actionTaken = true
-
-        // 2) Selkeä este suoraan edessä tai molemmilla puolilla
-    } else if (isObstacle(centerFront, CENTER_OBSTACLE_MM) ||
+    if (isObstacle(centerFront, CENTER_OBSTACLE_MM) ||
         (isObstacle(leftFront, SIDE_OBSTACLE_MM) && isObstacle(rightFront, SIDE_OBSTACLE_MM))) {
         obstacleEscape()
-        actionTaken = true
-
-        // 3) Este vain vasemmalla -> pieni korjaus oikealle
-    } else if (isObstacle(leftFront, SIDE_OBSTACLE_MM)) {
-        stopPidNow()
-        basic.pause(30)
-        turnRightDeg(SMALL_TURN_DEG)
-        basic.pause(30)
-        actionTaken = true
-
-        // 4) Este vain oikealla -> pieni korjaus vasemmalle
-    } else if (isObstacle(rightFront, SIDE_OBSTACLE_MM)) {
-        stopPidNow()
-        basic.pause(30)
-        turnLeftDeg(SMALL_TURN_DEG)
-        basic.pause(30)
         actionTaken = true
     }
 
     if (!actionTaken) {
         if (pidForwardActive) {
-            // Anna PID-ajolle hetki aikaa lähteä käyntiin
             if (input.runningTime() - forwardStartedAt > 700) {
-                // Jumitunnistus: käsky päällä, mutta nopeus lähes nolla pitkään
                 if (avgSpeed() <= STUCK_SPEED_CM_S) {
                     if (stuckSince == 0) {
                         stuckSince = input.runningTime()
                     }
-
                     if (input.runningTime() - stuckSince >= STUCK_TIME_MS) {
                         stuckEscape()
                         actionTaken = true
@@ -250,12 +233,10 @@ basic.forever(function () {
                     stuckSince = 0
                 }
 
-                // Kun PID-segmentti on valmis, vapauta tila uutta eteenajoa varten
                 if (avgSpeed() <= FINISHED_SPEED_CM_S) {
                     if (almostStoppedSince == 0) {
                         almostStoppedSince = input.runningTime()
                     }
-
                     if (input.runningTime() - almostStoppedSince >= FINISHED_TIME_MS) {
                         pidForwardActive = false
                         almostStoppedSince = 0
